3e5c646a67e79de820b390f516f7a04b1a38b040
Lam Bui Bao <lambuibao@outlook.com>
2020-11-25 12:13:04 +0700

    .

--- src/modules/commands/giveneso.rb
+++ src/modules/commands/giveneso.rb
@@ -6,9 +6,57 @@ module Bot::DiscordCommands
             neso = gets.chomp.downcase
             event << ''
                 if neso.content = "honoka"
-                    bot.create_reaction("<:HonokaNeso:661033812825997353>")
+                    bot.create_reaction(<:HonokaNeso:661033812825997353>)
                     bot.respond "Gave Honoka neso to #{neso.mentions}"
                 elsif neso.content = "kotori"
-                    bot.create_reaction("<:KotoriNeso:661033813077524481>")
+                    bot.create_reaction(<:KotoriNeso:661033813077524481>)
                     bot.respond "Gave Kotori neso to #{neso.mentions}"
-                
+                elsif neso.content = "umi"
+                    bot.create_reaction(<:UmiNeso:661033923102375986>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif neso.content = "hanayo"
+                    bot.create_reaction(<:HanayoNeso:661033812683390978>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif neso.content = "rin"
+                    bot.create_reaction(<:RinNeso:661033813933162536>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:MakiNeso:661033813333377062>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:EliNeso:661033545032007732>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif 
+                    bot.create_reaction(<:NozomiNeso:661033813560000517>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:NicoNeso:661033813585035314>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:ChikaNeso:661033545157967872>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:YouNeso:661033922863300619>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:RikoNeso:661033813769453591>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:RubyNeso:661033887971147796>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:HanamaruNeso:661033812834123776>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:UmiNeso:661033923102375986>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:UmiNeso:661033923102375986>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:UmiNeso:661033923102375986>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                elsif
+                    bot.create_reaction(<:UmiNeso:661033923102375986>)
+                    bot.respond "Gave Umi neso to #{neso.mentions}"
+                end
