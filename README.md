6d13497d1cddc178de7dfe9de4400e8c463731f9
Lam Bui Bao <lambuibao@outlook.com>
2020-11-26 14:42:48 +0700

    .

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 **rubybotrb**
 
+
 Discord bot based on Ruby Kurosawa, a Love Live! Sunshine character. This is written with discordrb, structure and stuffs are based on z64's [gemstone] (https://github.com/z64/gemstone).
 
 
 **Help section**
+
 
 `?apop`, `?cheese`, `?fpop`, `?ozo`: gives a string relating to each person
 
--- src/modules/commands/giveneso.rb
+++ src/modules/commands/giveneso.rb
@@ -43,10 +43,10 @@ module Bot::DiscordCommands
                 elsif neso.content.include? "ruby"
                     bot.create_reaction(<:RubyNeso:661033887971147796>)
                     bot.respond "Gave Ruby neso to #{neso.mentions}"
-                elsif neso.content.include? ["maru", "hanamaru"]
+                elsif neso.content.include? 'hanamaru', 'maru' 
                     bot.create_reaction(<:HanamaruNeso:661033812834123776>)
                     bot.respond "Gave Maru neso to #{neso.mentions}"
-                elsif neso.content.include? ["yoshiko", "yohane"]
+                elsif neso.content.include? 'yoshiko', 'yohane'
                     bot.create_reaction(<:YohaneNeso:661033922892791849>)
                     bot.respond "Gave Yohane neso to #{neso.mentions}"
                 elsif neso.content.include? "dia"
@@ -58,4 +58,42 @@ module Bot::DiscordCommands
                 elsif neso.content.include? "kanan"
                     bot.create_reaction(<:KananNeso:661033813245427732>)
                     bot.respond "Gave Kanan neso to #{neso.mentions}"
+                elsif neso.content.include? "leah"
+                    bot.create_reaction(<:LeahNeso:661033813085913123>)
+                    bot.respond "Gave Leah neso to #{neso.mentions}"
+                elsif neso.content.include? "sarah"
+                    bot.create_reaction(<:SarahNeso:661033922993324043>)
+                    bot.respond "Gave Kanan neso to #{neso.mentions}"
+                elsif neso.content.include? "tsubasa"
+                    bot.create_reaction(<:TsubasaNeso:666099332302372871>)
+                    bot.respond "Gave Tsubasa neso to #{neso.mentions}"
+                elsif neso.content.include? "anju"
+                    bot.create_reaction(<:AnjuNeso:666099347854721044>)
+                    bot.respond "Gave Anju neso to #{neso.mentions}"
+                elsif neso.content.include? "erena"
+                    bot.create_reaction(<:ErenaNeso:666099306301882373>)
+                    bot.respond "Gave Erena neso to #{neso.mentions}"
+                elsif neso.content.include? "rina"
+                    bot.create_reaction(<:RinaNeso:666112431176810496>)
+                    bot.respond "Gave Rina neso to #{neso.mentions}"
+                elsif neso.content.include? "ayumu"
+                    bot.respond "Ruby-chan doesn't have an Ayumu neso in her collection yet..."
+                elsif neso.content.include? "setsuna"
+                    bot.respond "Ruby-chan doesn't have an Setsuna neso in her collection yet..."
+                elsif neso.content.include? "ai"
+                    bot.respond "Ruby-chan doesn't have an Ai neso in her collection yet..."
+                elsif neso.content.include? "shizuku"
+                    bot.respond "Ruby-chan doesn't have an Shizuku neso in her collection yet..."
+                elsif neso.content.include? "kasumi"
+                    bot.respond "Ruby-chan doesn't have an Kasumi neso in her collection yet..."
+                elsif neso.content.include? "karin"
+                    bot.respond "Ruby-chan doesn't have an Karin neso in her collection yet..."
+                elsif neso.content.include? "emma"
+                    bot.respond "Ruby-chan doesn't have an Emma neso in her collection yet..."
+                elsif neso.content.include? "kanata"
+                    bot.respond "Ruby-chan doesn't have an Kanata neso in her collection yet..."
+                else 
+                    bot.respond "Is this a Love Live character? Ruby-chan doesn't know them..."
                 end
+            end
+        end
